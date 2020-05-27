45bb698d54482d2f4686275a7b19a9eff47ba206
Alex <alex.fernandez.canovas@gmail.com>
2020-05-28 00:15:58 +0200

    saving last changes and now testing the booking dates

--- middlewares/midEstablishment.js
+++ middlewares/midEstablishment.js
@@ -25,27 +25,59 @@ const checkIfIsPossibleBook = async (req, res, next) => {
   const dateParsed = new Date(day); //parseamos la fecha pàra que tenga el mismo modelo que el de la bbdd
 
   const establishment = await Establishment.findById(idEstablishment);
+  console.log("Los datos que tiene el establishment son", establishment.timetable)
   const { maximumCapacity, percentOfPeopleAllowed } = establishment.capacity;
-  let { timeAllowedPerBooking } = establishment.timetable;
+  let { timeAllowedPerBooking, startHourShift, finalHourShift } = establishment.timetable;
   timeAllowedPerBooking = parseInt(timeAllowedPerBooking) //pasamos la timeAllowed a number
   console.log("time allowed",timeAllowedPerBooking)
-  const percentOfUsersAllowedInTheEstablishmentInCertainTime = Math.round(
+  const percentOfUsersAllowedInTheEstablishmentInCertainTime = Math.round(  //sacamos el numero total de usuarios que se van a permitir por sesión.
     (maximumCapacity * percentOfPeopleAllowed) / 100,
   );
-  // console.log("el numero total de personas en un determinado espacio de tiempo son:",percentOfUsersAllowedInTheEstablishmentInCertainTime);
+
+    //saber los inicios de cada sesion de tiempo. para ello tenemos que encontrar los multiplos de horas hasta que cierra el local.
+    console.log("la jornada empieza alas",startHourShift);
+    startHourShift = parseInt(startHourShift)//los pasamos a minutos
+    finalHourShift = parseInt(finalHourShift)//los pasamos a minutos
+
+    //sabemos cuantas sesiones van a estar permitidas durante la jornada especificada en ese estableciemiento.(puede ser que no nos haga falta.)
+    const horasAbierto = finalHourShift-startHourShift
+    console.log("horas abierto", horasAbierto)
+    const sesionesTotales = (horasAbierto*60)/timeAllowedPerBooking;
+    console.log("sesiones totales", sesionesTotales)
+
+    // temp = startHourShift.split(':');
+    // console.log("me quedo con los minutos de la startHourShift del establishment", temp[1]);
+    // minutesOfEstablishment = parseInt(temp[1])//los pasamos a minutos
+    // const listOfIniciosDeSesionDeTiemposDisponibles = [];
+    // for (let index = 0; index < sesionesTotales; index++) {
+    //   minutesOfEstablishment = minutesOfEstablishment + timeAllowedPerBooking;
+    //   listOfIniciosDeSesionDeTiemposDisponibles.push(minutesOfEstablishment)
+    // }
+    console.log("lista que me devuelve",listOfIniciosDeSesionDeTiemposDisponibles)
+    // let cont = startHourShift;
+    // while (cont < finalHourShift) {
+    //   cont = cont + timeAllowedPerBooking;  //aqui hay que pensar en el tiempo. si es mas de 59 pasa de hora.
+    //   listOfIniciosDeSesionDeTiemposDisponibles.push(cont)
+    //   console.log("lista de inicios de sesion", listOfIniciosDeSesionDeTiemposDisponibles)
+    // }
+
+  console.log("el numero total de personas permitidas en un determinado espacio de tiempo en este establecimiento son:",percentOfUsersAllowedInTheEstablishmentInCertainTime);
   const findBookingsByDay = await Booking.find({ day: dateParsed, idEstablishment})//encontramos los que coinciden.
   console.log("bookings que coinciden en dia", findBookingsByDay)
   let countBookings = 0;
-  findBookingsByDay.map((booking)=>{
-    // booking.startHour = booking.startHour.split(':');
-    console.log("booking starthour:", booking.startHour);
-    //saber si lo que ha introducido el usuario cumple con el hotario
-    //por ejempo si lo ha puesto a las 10.20 hasta las 11 perf porque está denrto de la misma sesion-
-    // let timeRest = timeAllowedPerBooking - duration;
-    // console.log("resta dela operacion:", timeRest)
-    // booking.startHour = booking.startHour - timeRest;
-    // console.log("Finalmente ha quedado redondeado a :", booking.startHour)
+  findBookingsByDay.map((booking)=>{ //con esto recorremos todas las bookings de ese dia buscando que hayan empezado en la misma sesion
+    // punto = booking.startHour.split(':');
+    // console.log("me quedo con los minutos de la startHur introducido por el usuario:", punto[1]);
+    // minutesOfBooking = parseInt(punto[1])//los pasamos a minutos
+    // console.log("minutos en numero(alomejor esto no hace falta)",minutesOfBooking)
+    // //creo que puede ser una idea luego desde el front obligar al usuario que ponga la duration maxima permitida apartir de la hora que reserva.
+    // suma = minutesOfBooking + duration
+    // console.log("la suma de los minutos entre que entra y la duracion es", suma)
+    // resta = suma - timeAllowedPerBooking;
+    // console.log("el resultado de la resta entre los minutos que ha puesto y los permitidos son", resta)
 
+    //falta comprobar si el resultado de esta ultima operacion se considera un tiempo de inicio de sesion: 9.00, 9.20, 9.40
+    //en ese caso cuenta que está bien y el contador suma
     if(booking.startHour === '09:00'){
       countBookings= countBookings +1;
     }
